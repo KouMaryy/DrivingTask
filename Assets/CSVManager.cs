e066fad134597713bcffe7129f7bda9430ad625d
KouMaryy <36680874+KouMaryy@users.noreply.github.com>
2026-03-30 23:43:48 +0200

    implement real-time SDT scoring logic and UI feedback
    - CalculateScore using SDT payoff matrix
    - Linked score to CSV logging for participant data analysis.
    - Increased fog density for enhanced uncertainty in high-intensity trials.

--- Assets/CSVManager.cs
+++ Assets/CSVManager.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 public static class CSVManager
 {
-    public static void SaveTrial(string fileName, int id, string weather, bool lying, string action, bool intervened, int numInterventions, float reactionTime, string aiSuggestedlLaneLabel, string finalLane, bool success, string dangerousObstacle, string safeObstacle)
+    public static void SaveTrial(string fileName, int id, string weather, bool lying, string action, bool intervened, int numInterventions, float reactionTime, string aiSuggestedlLaneLabel, string finalLane, bool success, int currentScore, string dangerousObstacle, string safeObstacle)
     {
         string fullFileName = fileName + ".csv";
         string filePath = Path.Combine(Application.dataPath, fullFileName);
@@ -11,7 +11,7 @@ public static class CSVManager
         // If the file doesn't exist, create it and write the header
         if (!File.Exists(filePath))
         {
-            string header = "Trial_ID,Weather_Condition,AI_Lying,AI_Action,Intervened,Num_Interventions,Reaction_Time_Seconds,AI_Suggested_Lane,Final_Lane,Success,Dangerous_Object,Safe_Object,Timestamp" + System.Environment.NewLine;
+            string header = "Trial_ID,Weather_Condition,AI_Lying,AI_Action,Intervened,Num_Interventions,Reaction_Time_Seconds,AI_Suggested_Lane,Final_Lane,Success,Total_Score, Dangerous_Object,Safe_Object,Timestamp" + System.Environment.NewLine;
             File.WriteAllText(filePath, header);
         }
 
@@ -21,7 +21,7 @@ public static class CSVManager
         string reactionStr = (reactionTime > 0) ? reactionTime.ToString("F3") : "0";
 
         // Create a CSV row with the trial data
-        string row = $"{id},{weather},{lying},{action},{intervened},{numInterventions},{reactionStr},{aiSuggestedlLaneLabel},{finalLane},{success},{dangerousObstacle},{safeObstacle},{timestamp}" + System.Environment.NewLine;
+        string row = $"{id},{weather},{lying},{action},{intervened},{numInterventions},{reactionStr},{aiSuggestedlLaneLabel},{finalLane},{success}, {currentScore},{dangerousObstacle},{safeObstacle},{timestamp}" + System.Environment.NewLine;
         // Append the row to the CSV file
         File.AppendAllText(filePath, row);
     }
